a7d3989b70cb9514e2d819e7e3a3d4889a52d499
Khamaruzaman <riahanathyahya@gmail.com>
2026-03-31 23:54:28 +0530

    update getUsers method in UserController to return AuthResponse wrapper for consistent API responses

--- src/main/java/com/example/BlogApp/controller/UserController.java
+++ src/main/java/com/example/BlogApp/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.BlogApp.controller;
 
+import com.example.BlogApp.DTO.AuthResponse;
 import com.example.BlogApp.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,20 @@ public class UserController {
     UserService userService;
 
     @GetMapping
-    public List<String> getUsers() {
-        return userService.findAll();
+    public ResponseEntity<AuthResponse<List<String>>> getUsers() {
+        try {
+            AuthResponse<List<String>> response = new AuthResponse<>();
+            response.setSuccess(true);
+            response.setMessage("Users retrieved successfully");
+            response.setData(userService.findAll());
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            AuthResponse<List<String>> response = new AuthResponse<>();
+            response.setSuccess(false);
+            response.setMessage("Users retrieved failed: " + e.getMessage());
+            response.setData(null);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
     }
 
 }
