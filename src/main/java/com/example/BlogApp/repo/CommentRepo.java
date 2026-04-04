44596c8417fb7e37b35483cdc4ad0b3378a5c99f
Khamaruzaman <riahanathyahya@gmail.com>
2026-04-04 22:00:54 +0530

    update CommentRepo and PostRepo to return lists of comments and posts by postId, authorId, and published status for improved data retrieval

--- src/main/java/com/example/BlogApp/repo/CommentRepo.java
+++ src/main/java/com/example/BlogApp/repo/CommentRepo.java
@@ -3,9 +3,10 @@ package com.example.BlogApp.repo;
 import com.example.BlogApp.model.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepo extends MongoRepository<Comment, UUID> {
-    Comment findByPostId(UUID postId);
-    Comment findByAuthorId(UUID authorId);
+    List<Comment> findByPostId(UUID postId);
+    List<Comment> findByAuthorId(UUID authorId);
 }
--- src/main/java/com/example/BlogApp/repo/PostRepo.java
+++ src/main/java/com/example/BlogApp/repo/PostRepo.java
@@ -3,10 +3,11 @@ package com.example.BlogApp.repo;
 import com.example.BlogApp.model.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PostRepo extends MongoRepository<Post, UUID> {
-    Post findByTitleContaining(String title);
-    Post findByAuthorId(UUID authorId);
-    Post findByPublishedTrue(boolean published);
+    List<Post> findByTitleContaining(String title);
+    List<Post> findByAuthorId(UUID authorId);
+    List<Post> findByPublishedTrue(boolean published);
 }
